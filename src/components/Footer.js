f3742bef2eccc9eeb05ba9b1bd0c93a6295384f4
Felix Kiptoo <felixkpt@gmail.com>
2022-04-20 16:13:38 +0300

    Modified footer sytles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,39 @@
 const Footer = () => {
     return (
         <footer>
-            <div className="flex-footer w-full">
-                <div className="flex w-full">
-                    <h3>Section 1</h3>
-                    <p>
-                        Lorem ipsum
-                    </p>
+            <div className="flex flex-wrap w-full">
+                <div className="flex flex-col w-1/4 border">
+                    <h2 className="text-decoration-underline text-gray-100">Our Brand</h2>
+                    <ul className="footer-list">
+                        <li><a href="#">Homepage</a></li>
+                        <li><a href="#">Who we are</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+
                 </div>
-                <div className="flex w-full">
-                    <h3>Section 2</h3>
-                    <p>
-                        Lorem ipsum
-                    </p>
+                <div className="flex flex-col w-1/4 border">
+                    <h2 className="text-decoration-underline text-gray-100">Services</h2>
+                    <ul className="footer-list">
+                        <li><a href="#">Latest vehicles listing</a></li>
+                        <li><a href="#">Best prices suggestions</a></li>
+                        <li><a href="#">Onsale portal</a></li>
+                    </ul>
                 </div>
-                <div className="flex w-full">
-                    <h3>Section 3</h3>
-                    <p>
-                        Lorem ipsum
-                    </p>
+                <div className="flex flex-col w-1/4 border">
+                    <h2 className="text-decoration-underline text-gray-100">Faqs</h2>
+                    <ul className="footer-list">
+                        <li><a href="#">How to join</a></li>
+                        <li><a href="#">How to Buy</a></li>
+                        <li><a href="#">Membership Fee</a></li>
+                    </ul>
+                </div>
+                <div className="flex flex-col w-1/4 border">
+                    <h2 className="text-decoration-underline text-gray-100">About</h2>
+                    <ul className="footer-list">
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Privacy</a></li>
+                        <li><a href="#">Terms</a></li>
+                    </ul>
                 </div>
             </div>
         </footer>
